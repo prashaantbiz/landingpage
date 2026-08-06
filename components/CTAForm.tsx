2b5ab1cb1dc3bb1dc21fbd05128d85182e5b2444
prashaantbiz <prashaant.on.work@gmail.com>
2026-08-07 01:39:15 +0545

    Update CTA form scrollbar

--- components/CTAForm.tsx
+++ components/CTAForm.tsx
@@ -3,7 +3,7 @@
             src="https://7f6523a8.sibforms.com/v2/serve/MUIFAMcqUXGvMn2jGxjoMjse65JhmEjJijqE7gF_u63dKDO6-g061trFly-dWlDCmBK2b8krw-IohrPkaunz1lNotF9HTHiAQhRKkxDcVUmmFwGLDD3zohbd1buCLn5mnOl746rwnvSMGJsLwcQ1kmhr526abZGjdqAh5FDtzdazWHEPtdjZp2gMJ7T_onchlXlxvNAFkOVQnhi1SA=="
             className="brevo-frame"
             frameBorder="0"
-            scrolling="auto"
+            scrolling="no"
             allowFullScreen
           />
         </div>
