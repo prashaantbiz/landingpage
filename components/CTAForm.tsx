cb0d793aa2c10ed6493890e0638a7325f48f876f
prashaantbiz <prashaant.on.work@gmail.com>
2026-08-07 02:18:44 +0545

    Make Brevo form responsive

--- components/CTAForm.tsx
+++ components/CTAForm.tsx
@@ -1,21 +1,27 @@
 export function CTAForm() {
   return (
-<section className="flex justify-center py-12">
-    <iframe
-      title="Brevo Form"
-      src="https://7f6523a8.sibforms.com/v2/serve/MUIFAMcqUXGvMn2jGxjoMjse65JhmEjJijqE7gF_u63dKDO6-g061trFly-dWlDCmBK2b8krw-IohrPkaunz1lNotF9HTHiAQhRKkxDcVUmmFwGLDD3zohbd1buCLn5mnOl746rwnvSMGJsLwcQ1kmhr526abZGjdqAh5FDtzdazWHEPtdjZp2gMJ7T_onchlXlxvNAFkOVQnhi1SA=="
-      width="540"
-      height="1200"
-      frameBorder={0}
-      scrolling="no"
-      allowFullScreen
+    <section
+      className="flex justify-center py-8 px-2"
       style={{
-        display: "block",
-        margin: "0 auto",
-        maxWidth: "100%",
-        border: "none",
+        width: "100%",
       }}
-    />
-  </section>
+    >
+      <iframe
+        title="Brevo Form"
+        src="https://7f6523a8.sibforms.com/v2/serve/MUIFAMcqUXGvMn2jGxjoMjse65JhmEjJijqE7gF_u63dKDO6-g061trFly-dWlDCmBK2b8krw-IohrPkaunz1lNotF9HTHiAQhRKkxDcVUmmFwGLDD3zohbd1buCLn5mnOl746rwnvSMGJsLwcQ1kmhr526abZGjdqAh5FDtzdazWHEPtdjZp2gMJ7T_onchlXlxvNAFkOVQnhi1SA=="
+        width="100%"
+        height="1200"
+        frameBorder={0}
+        scrolling="no"
+        allowFullScreen
+        style={{
+          display: "block",
+          width: "100%",
+          maxWidth: "700px",
+          border: "none",
+          margin: "0 auto",
+        }}
+      />
+    </section>
   );
 }
