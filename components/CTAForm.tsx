91715bf32eb92c2467dc275cdc49f3b951269f66
prashaantbiz <prashaant.on.work@gmail.com>
2026-08-07 02:11:04 +0545

    Update lockfile after Next.js build

--- components/CTAForm.tsx
+++ components/CTAForm.tsx
@@ -1,21 +1,21 @@
-export default function CTAForm() {
+export function CTAForm() {
   return (
-    <section className="flex justify-center py-12">
-      <iframe
-        title="Brevo Form"
-        src="https://7f6523a8.sibforms.com/v2/serve/MUIFAMcqUXGvMn2jGxjoMjse65JhmEjJijqE7gF_u63dKDO6-g061trFly-dWlDCmBK2b8krw-IohrPkaunz1lNotF9HTHiAQhRKkxDcVUmmFwGLDD3zohbd1buCLn5mnOl746rwnvSMGJsLwcQ1kmhr526abZGjdqAh5FDtzdazWHEPtdjZp2gMJ7T_onchlXlxvNAFkOVQnhi1SA=="
-        width="540"
-        height="1200"
-        frameBorder={0}
-        scrolling="no"
-        allowFullScreen
-        style={{
-          display: "block",
-          margin: "0 auto",
-          maxWidth: "100%",
-          border: "none",
-        }}
-      />
-    </section>
+<section className="flex justify-center py-12">
+    <iframe
+      title="Brevo Form"
+      src="https://7f6523a8.sibforms.com/v2/serve/MUIFAMcqUXGvMn2jGxjoMjse65JhmEjJijqE7gF_u63dKDO6-g061trFly-dWlDCmBK2b8krw-IohrPkaunz1lNotF9HTHiAQhRKkxDcVUmmFwGLDD3zohbd1buCLn5mnOl746rwnvSMGJsLwcQ1kmhr526abZGjdqAh5FDtzdazWHEPtdjZp2gMJ7T_onchlXlxvNAFkOVQnhi1SA=="
+      width="540"
+      height="1200"
+      frameBorder={0}
+      scrolling="no"
+      allowFullScreen
+      style={{
+        display: "block",
+        margin: "0 auto",
+        maxWidth: "100%",
+        border: "none",
+      }}
+    />
+  </section>
   );
 }
